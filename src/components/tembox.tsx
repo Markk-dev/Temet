840c029905789dc7b99fa4700dbede68f78368b1
Markk-dev <markvincent.dev@gmail.com>
2025-08-29 20:07:47 +0800

    Changes made on  Tembox

--- src/components/tembox.tsx
+++ src/components/tembox.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { HardDrive, Palette } from "lucide-react";
+import { HardDrive, Palette, BotMessageSquare } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { useTemboxModal } from "@/features/tembox/hooks/use-tembox-modal";
@@ -16,7 +16,7 @@ export const TemBox = () => {
                 <p className="text-sm uppercase text-neutal-500"> Assets </p>
             </div>
             
-            <div className="w-full max-w-sm">
+            <div className="w-full max-w-sm flex flex-col gap-y-1">
                 <div className="flex rounded-lg overflow-hidden border border-border">
                     <button 
                         onClick={open}  
@@ -38,8 +38,20 @@ export const TemBox = () => {
                         )}
                     >
                         <Palette className="w-4 h-4" />
-                        UI Tool
+                        Canvas
                     </div>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button 
+                        onClick={open}  
+                        className={cn(
+                            "text-sm flex-1 rounded-md border border-gray-300 bg-transparent text-gray-700 hover:bg-gray-50 hover:border-gray-400 transition-all flex items-center justify-center gap-2 px-4 py-2 mt-2",
+                            pathname.includes('/tembox') && "border-gray-400  bg-gray-50"
+                        )}
+                    >
+                        <BotMessageSquare className="w-4 h-4" />
+                        AI Assistant
+                    </button>
                 </div>
             </div>
         </div>
